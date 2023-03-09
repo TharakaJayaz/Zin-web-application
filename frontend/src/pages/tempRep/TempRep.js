95bb1dedee02a9da9c678d7dfae2544afb8754a9
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-03-09 15:01:20 +0530

    7 th commit
    add more features to temp reps page

--- frontend/src/pages/tempRep/TempRep.js
+++ frontend/src/pages/tempRep/TempRep.js
@@ -10,9 +10,10 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { NavLink } from "react-router-dom";
+import RepSearch from "./RepSearch";
 const TempRep = () => {
   const [tempReps, setTempReps] = useState([]);
-  const [routingValue, setroutingValue] = useState(false);
+  const [enableValue, setEnableValue] = useState(false);
   // const [repValLength, setRepValLength] = useState();
 
   useEffect(() => {
@@ -20,33 +21,39 @@ const TempRep = () => {
       try {
         const res = await axios.get("http://localhost:8800/reps");
         setTempReps(res.data);
-        if (tempReps == null) {
-          setroutingValue(true);
-        }
-        console.log(res.data);
       } catch (err) {
         console.log(err);
       }
     };
 
     fetchAllTempReps();
-    console.log(tempReps)
+    if (tempReps.length === 0) {
+    }
+    console.log(tempReps.length);
+
+    console.log(tempReps);
     // setRepValLength(tempReps.length);
   }, []);
+
+  const buttonHandler = () => {
+    setEnableValue(true);
+  };
+
   return (
     <div className={classes.tempRep_main_div}>
       <NavbarAdmin
         className={classes.navBarStyle}
         buttonstyle={classes.searchButtonStyle}
       />
-      {!routingValue && (
+      {!enableValue && <button className={classes.routing_button} onClick={buttonHandler}>View Reps</button>}
+      {!enableValue && (
         <div className={classes.tempReps_card_div}>
           <Swiper
             className={classes.tempReps_card_swiper}
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={0}
             // slidesPerView={repValLength <3? repValLength : 3}
-            slidesPerView = {2}
+            slidesPerView={2}
             // navigation
             pagination={{ clickable: true }}
             // scrollbar={{ draggable: true }}
@@ -55,9 +62,7 @@ const TempRep = () => {
           >
             {tempReps.map((tempR) => (
               <SwiperSlide
-                className={
-                  classes.tempReps_card_swipeSlide
-                }
+                className={classes.tempReps_card_swipeSlide}
                 key={tempR.RID}
               >
                 <RepCard
@@ -68,7 +73,7 @@ const TempRep = () => {
                   mobile={tempR.phone}
                   rdate={tempR.registration_date}
                   rid={tempR.RID}
-                  password = {tempR.password}
+                  password={tempR.password}
                 />
               </SwiperSlide>
 
@@ -77,13 +82,11 @@ const TempRep = () => {
           </Swiper>
         </div>
       )}
-      {!routingValue && (
-        <div className={classes.tempReps_route_button_div}>
-          <NavLink to="view_reps">
-            <button>View Reps</button>
-          </NavLink>
-        </div>
-      )}
+      {enableValue && <RepSearch />}
+
+      {/* <div className={classes.tempReps_route_button_div}>
+        {!enableValue && <button onClick={buttonHandler}>View Reps</button>}
+      </div> */}
     </div>
   );
 };
